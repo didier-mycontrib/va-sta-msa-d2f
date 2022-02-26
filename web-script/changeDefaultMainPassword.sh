37da057016c09f09b2f69f80c8c23247d1422ff7
didierDefrance <didier@d-defrance.fr>
2022-02-26 11:32:16 +0100

    s

--- web-script/changeDefaultMainPassword.sh
+++ web-script/changeDefaultMainPassword.sh
@@ -1,12 +1,9 @@
 LOCAL_SERVICE_URL=http://localhost:8232/user-api/private/user
 echo "new password for main admin (didier):"
 read PWD
-
-jsonDATA="{ \"username\" : \"didier\" , \"password\" : \"${PWD}\"  , \"roles\" : \"admin\"}"
-  
-curl -i -X PUT \
-  -H "Content-Type: application/json" \
-  --url $LOCAL_SERVICE_URL \
-  --data $jsonDATA
-  
-curl http://localhost:8232/user-api/private/user
+echo "new password will be ${PWD}"
+jsonDATA="{ \"username\" : \"didier\" , \"password\" : \"${PWD}\"  , \"roles\" : \"admin\" }"
+echo $jsonDATA  
+curl $LOCAL_SERVICE_URL -X PUT -H "Content-Type: application/json" -d "{ \"username\" : \"didier\" , \"password\" : \"${PWD}\"  , \"roles\" : \"admin\" }"
+echo "in mongo database updated values are :"  
+curl http://localhost:8232/user-api/private/user/didier
