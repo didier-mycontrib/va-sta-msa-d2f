c2e02bad9ab2bcead48831165ec7d5e0eab650ba
didierDefrance <didier@d-defrance.fr>
2022-03-04 19:09:49 +0100

    s

---
+++ web-script/init-dockers-contents.sh
@@ -0,0 +1,9 @@
+PWD=`pwd` 
+export BASE=$HOME/va-sta-msa-d2f
+cd $BASE
+md docker-contents
+cd docker-contents
+git clone https://github.com/didier-mycontrib/ddc-api
+git clone https://github.com/didier-mycontrib/qcm-api
+git clone https://github.com/didier-mycontrib/user-api
+cd $PWD
