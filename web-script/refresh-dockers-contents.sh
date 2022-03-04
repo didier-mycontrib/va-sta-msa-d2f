9d703f704dfd9f1fc8b84c54cb6bce11b6ab28cf
didierDefrance <didier@d-defrance.fr>
2022-03-04 20:36:40 +0100

    s

--- web-script/refresh-dockers-contents.sh
+++ web-script/refresh-dockers-contents.sh
@@ -1,9 +1,9 @@
 PWD=`pwd` 
 export BASE=$HOME/va-sta-msa-d2f
 cd $BASE/docker-contents/ddc-api
-git pull
+git pull main
 cd $BASE/docker-contents/qcm-api
-git pull
+git pull main
 cd $BASE/docker-contents/user-api
-git pull
+git pull main
 cd $PWD
