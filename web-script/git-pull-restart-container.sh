b922e492655cfc13a4203fe86a11a6d62d748266
didierDefrance <didier@d-defrance.fr>
2022-03-05 12:16:34 +0100

    s

--- web-script/git-pull-restart-container.sh
+++ web-script/git-pull-restart-container.sh
@@ -3,8 +3,8 @@ export BASE=$HOME/va-sta-msa-d2f
 cd $BASE/docker-contents/ddc-api
 git pull 1>&1 | grep "Already up-to-date."
 if [ ! $? -eq 0 ]; then
-	echo "need to restart container"
+	echo "Already up-to-date."
 else 
-    echo "Already up-to-date."
+    echo "need to restart container"; sh $BASE/dock-script/dcr-backend-ddc.sh
 fi
 cd $PWD
