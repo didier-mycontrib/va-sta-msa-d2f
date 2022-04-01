267d7e9509f8dbc13be4517bffd7c08fe60b027c
Didier Defrance <didier@d-defrance.fr>
2022-04-01 16:03:45 +0200

    s

--- dock-script/dcr-kong-api-gateway.sh
+++ dock-script/dcr-kong-api-gateway.sh
@@ -1,6 +1,6 @@
 #adcr : absolute docker compose rebuild/restart (with absolute path)
 sh /base/conf-docker/kong-api-gateway/adcr.sh
-cp /base/conf-docker/kong-api-gateway/docker-compose-after-first-start.yml sh /base/conf-docker/kong-api-gateway/docker-compose.yml
+cp /base/conf-docker/kong-api-gateway/docker-compose-after-first-start.yml  /base/conf-docker/kong-api-gateway/docker-compose.yml
 echo "waiting 30s before reinit-vps-gateway-access.sh"
 sleep 30
 sh /base/dock-script/reinit-vps-gateway-access.sh
