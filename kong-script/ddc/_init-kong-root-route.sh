b5f8e0593ce1e394fa554efb9f97a0c4b760cc2b
DidierDefrance <didier@d-defrance.fr>
2023-04-23 23:12:31 +0200

    s

--- kong-script/ddc/_init-kong-root-route.sh
+++ kong-script/ddc/_init-kong-root-route.sh
@@ -22,7 +22,8 @@ curl -i -X POST \
   --data "hosts[]=${HOST3}" \
   --data 'hosts[]=localhost' \
   --data "paths[]=/index.html" \
-  --data "paths[]=/$"
+  --data "paths[]=/" \
+  --data "paths[]="
   
 #  --data "paths[]=/" replaced by --data "paths[]=/other" (just a test)
 # because / may be too much
