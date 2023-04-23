616cc54ac4a5da41c8bf9187a9651fd5e324d821
DidierDefrance <didier@d-defrance.fr>
2023-04-23 10:49:32 +0200

    s

--- dock-script/dcr-keycloak.sh
+++ dock-script/dcr-keycloak.sh
@@ -1,3 +1,7 @@
+# A ameliorer :      
+sudo mkdir -p /var/my-docker-volumes/keycloak/data
+sudo chmod ag+w  /var/my-docker-volumes/keycloak/data
+
 #adcr : absolute docker compose rebuild/restart (with absolute path)
 sh /base/conf-docker/keycloak/adcr.sh
 
