4eb4a61d18fe5856499b6f91c7cfab50c9387760
DidierDefrance <didier@d-defrance.fr>
2023-05-05 16:13:21 +0200

    s

--- web-script/init-local-git-reporitories.sh
+++ web-script/init-local-git-reporitories.sh
@@ -6,9 +6,10 @@ mkdir local-git-repositories
 cd local-git-repositories
 git clone https://github.com/didier-mycontrib/ddc-api
 git clone https://github.com/didier-mycontrib/qcm-api
-git clone https://github.com/didier-mycontrib/tp-api
+
 git clone https://github.com/didier-mycontrib/user-api
 git clone https://github.com/didier-mycontrib/ddc-app
 git clone https://github.com/didier-mycontrib/qcm-app
 git clone https://github.com/didier-mycontrib/tp-app
 cd $PWD
+git clone https://github.com/didier-mycontrib/tp-api
