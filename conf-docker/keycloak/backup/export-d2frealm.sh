9f64ec93bc9047882247ce3a8fd12215f78b4849
DidierDefrance <didier@d-defrance.fr>
2023-05-05 15:37:49 +0200

    s

--- conf-docker/keycloak/backup/export-d2frealm.sh
+++ conf-docker/keycloak/backup/export-d2frealm.sh
@@ -1,11 +1,14 @@
 echo "export keycloak d2frealm"
 export KC_CONTAINER_NAME=keycloak
 export REALM=d2frealm
+export HOST_BKUP_DIR=/var/my-docker-volumes/keycloak/data/backup/${REALM}
+sudo mkdir -p ${HOST_BKUP_DIR}
+sudo chmod 777 ${HOST_BKUP_DIR}
 
 #docker container exec -ti ${KC_CONTAINER_NAME} sh
 
 docker exec  ${KC_CONTAINER_NAME} \
       /opt/keycloak/bin/kc.sh  export --dir /opt/keycloak/data/backup/${REALM} --realm ${REALM}
 	  
-echo "resultats normalement dans /var/my-docker-volumes/keycloak/data/backup"
-ls -l /var/my-docker-volumes/keycloak/data/backup
+echo "resultats normalement dans ${HOST_BKUP_DIR}"
+ls -l ${HOST_BKUP_DIR}
--- conf-docker/keycloak/backup/export-myrealm.sh
+++ conf-docker/keycloak/backup/export-myrealm.sh
@@ -1,11 +1,14 @@
 echo "export keycloak myrealm"
 export KC_CONTAINER_NAME=keycloak
 export REALM=myrealm
+export HOST_BKUP_DIR=/var/my-docker-volumes/keycloak/data/backup/${REALM}
+sudo mkdir -p ${HOST_BKUP_DIR}
+sudo chmod 777 ${HOST_BKUP_DIR}
 
 #docker container exec -ti ${KC_CONTAINER_NAME} sh
 
 docker exec  ${KC_CONTAINER_NAME} \
       /opt/keycloak/bin/kc.sh  export --dir /opt/keycloak/data/backup/${REALM} --realm ${REALM}
 	  
-echo "resultats normalement dans /var/my-docker-volumes/keycloak/data/backup"
-ls -l /var/my-docker-volumes/keycloak/data/backup
+echo "resultats normalement dans ${HOST_BKUP_DIR}"
+ls -l ${HOST_BKUP_DIR}
