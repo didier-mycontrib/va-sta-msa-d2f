c16ceed999bba0f4bb14e10b6604cc12924d7dcf
DidierDefrance <didier@d-defrance.fr>
2023-05-09 19:16:45 +0200

    s

--- web-script/copy-frontends-content.sh
+++ web-script/copy-frontends-content.sh
@@ -1,1 +1,3 @@
-sudo cp -r  /base/local-git-repositories/ddc-app/dist/ddc-app /base/conf-docker/myNginx/my-nginx/ddc-app  
+sudo cp -r  /base/local-git-repositories/ddc-app/dist/ddc-app /base/conf-docker/myNginx/my-nginx/ddc-app
+sudo cp -r  /base/local-git-repositories/tp-app/dist/tp-app /base/conf-docker/myNginx/my-nginx/tp-app
+sudo cp -r  /base/local-git-repositories/qcm-app/dist/qcm-app /base/conf-docker/myNginx/my-nginx/qcm-app 
