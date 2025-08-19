ac55209949fd4afc53d496573f0a19b73ab2dbbc
Didier_Defrance <didier@d-defrance.fr>
2025-08-19 12:39:06 +0200

    s

---
+++ conf-docker/backend-qcm/docker-image-pull-backend.sh
@@ -0,0 +1,2 @@
+#docker image rm didierdefrance69/qcm_api:1
+docker image pull didierdefrance69/qcm_api:1
--- conf-docker/backend-qcm/docker-image-rm-frontend.sh
+++
@@ -1,2 +0,0 @@
-#docker image rm backend-qcm_qcm-api
-docker image rm didierdefrance69/qcm_api:1
