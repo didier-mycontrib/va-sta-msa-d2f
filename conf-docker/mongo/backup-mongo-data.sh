b35ebb5db44f00e91045294d233f03820850681e
Didier_Defrance <didier@d-defrance.fr>
2025-08-19 11:04:46 +0200

    s

---
+++ conf-docker/mongo/backup-mongo-data.sh
@@ -0,0 +1,2 @@
+#sauvegarde des données de mongo après arrêt du container:
+tar cvf /var/my-backups/mongo_data_db.tar   /var/my-docker-volumes/mongo/data/db
---
+++ conf-docker/mongo/restore-mongo-data.sh
@@ -0,0 +1,2 @@
+#restauration des données sauvegardées de mongo après arrêt du container:
+tar xvf /var/my-backups/mongo_data_db.tar
