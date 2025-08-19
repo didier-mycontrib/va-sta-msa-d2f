bcc9687a7953a9d51b13833140e3a8ade247da62
Didier_Defrance <didier@d-defrance.fr>
2025-08-19 15:02:28 +0200

    s

--- conf-docker/mongo/backup-mongo-data.sh
+++ conf-docker/mongo/backup-mongo-data.sh
@@ -1,3 +1,4 @@
 #sauvegarde des données de mongo après arrêt du container:
 sudo rm -rf /var/my-docker-volumes/mongo/data/db/diagnostic.data
 sudo tar -Pcvf /var/my-backups/mongo_data_db.tar   /var/my-docker-volumes/mongo/data/db
+#pour vérifier le contenu de l'archive: tar -tf /var/my-backups/mongo_data_db.tar
