67d3c77035ac1907a12624b7f0269fb75dd25016
Didier_Defrance <didier@d-defrance.fr>
2025-08-21 11:50:37 +0200

    s

---
+++ conf-docker/backend-tp/docker-container-exec-sh.sh
@@ -0,0 +1,6 @@
+docker exec -it backend-tp-api sh
+# echo $MONGODB_HOSTNAME
+# echo $MONGODB_PORT
+# echo $MONGODB_USERNAME
+# echo $MONGODB_PASSWORD
+# echo $MONGODB_PASSWORD_FILE
---
+++ conf-docker/backend-tp/docker-image-pull-backend.sh
@@ -0,0 +1,2 @@
+#docker image rm didierdefrance69/tp_api:1
+docker image pull didierdefrance69/tp_api:1
