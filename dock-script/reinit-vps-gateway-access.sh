6c35d90fc5d8d745966c46a2ae1de47b72155990
Didier_Defrance <didier@d-defrance.fr>
2025-08-27 19:16:21 +0200

    s

--- dock-script/reinit-vps-gateway-access.sh
+++ dock-script/reinit-vps-gateway-access.sh
@@ -1,7 +1,2 @@
-#sudo ufw-docker delete allow kong-gateway
-
-#sudo ufw-docker allow kong-gateway 80
-#sudo ufw-docker allow kong-gateway 443
-
 sudo ufw-docker allow myNginx 80
 sudo ufw-docker allow myNginx 443
--- web-script/git-pull-restart-container.sh
+++ web-script/git-pull-restart-container.sh
@@ -1,13 +1,2 @@
-PWD=`pwd` 
-#export BASE=$HOME/va-sta-msa-d2f
-export BASE=/base
-cd $BASE/docker-contents/ddc-api
-git pull > lastGitPullRes.txt 2>&1
-ALREADYUPTODATE="Already up to date." 
-ISUPTODATE=`grep -c "$ALREADYUPTODATE"  lastGitPullRes.txt`
-if [ $ISUPTODATE -eq 1 ]; then
-	echo "Already up to date."
-else 
-    echo "need to restart container"; sh $BASE/dock-script/dcr-backend-ddc.sh
-fi
-cd $PWD
+# ddc-api , dcr-backend-ddc is now as ssr and build as docker image via jenkins
+# ==> nothing to do 
