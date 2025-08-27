80719cfbece953c33e583e98abb58a2ebfb249ee
Didier_Defrance <didier@d-defrance.fr>
2025-08-27 21:04:55 +0200

    s

--- dock-script/prepare_volumes_and_symlinks.sh
+++ dock-script/prepare_volumes_and_symlinks.sh
@@ -4,6 +4,13 @@ sudo chmod 755 /var/my-docker-volumes
 sudo mkdir -p /var/my-docker-volumes/keycloak/data
 sudo chmod ag+w  /var/my-docker-volumes/keycloak/data 
 
+sudo mkdir -p /var/my-docker-volumes/mongo/data/db
+sudo chmod ag+w  /var/my-docker-volumes/keycloak/data 
+
+sudo mkdir -p /var/my-docker-volumes/ddc-app-posts/images
+sudo chmod ag+w /var/my-docker-volumes/ddc-app-posts
+sudo chmod ag+w /var/my-docker-volumes/ddc-app-posts/images
+
 sudo mkdir -p /var/my-docker-volumes/keycloak/data/backup
 sudo chmod 755 /var/my-docker-volumes/keycloak/data/backup
 sudo ln -s  /var/my-docker-volumes/keycloak/data/backup /base/conf-docker/keycloak/backup/backup-symlink
