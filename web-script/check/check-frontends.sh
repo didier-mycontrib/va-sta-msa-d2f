5d4776272891f5b83fa99014ecc344e22b17de41
Didier_Defrance <didier@d-defrance.fr>
2025-08-27 21:10:55 +0200

    s

--- web-script/check/check-frontends.sh
+++ web-script/check/check-frontends.sh
@@ -1,4 +1,6 @@
 echo "************ ddc-app  ***************"
-curl http://localhost:8888/ddc-app/index.html
+#curl http://localhost:4001/ddc-app/index.html
+curl https://www.d-defrance.fr/ddc-app/index.html
 echo "************ qcm-app  ***************"
-curl http://localhost:8888/qcm-app/index.html
+#curl http://localhost:4009/qcm-app/index.html
+curl https://www.d-defrance.fr/qcm-app/index.html
